2e01d73f83b0ee83ed5d11d9ec2c1e614c55206a
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-08-24 16:23:44 +0100

    added more notes and lessons

--- querystringsplit.js
+++ querystringsplit.js
@@ -5,7 +5,12 @@ var url = require('url');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type':'text/html'});
     var q  = url.parse(req.url, true).query;
-    var txt = q.year + "" + q.month;
+    var txt = q.year + " " + q.month;
     res.end(txt);
-
 }).listen(8080);
+
+//use as local host
+//http://localhost:8080/?year=2022&month=August
+
+
+
